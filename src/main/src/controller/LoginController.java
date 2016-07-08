6587e1e2f99a6c283417255d15c8f3418e3a13e5
joorhy <joorhy@gmail.com>
2016-07-08 18:02:04 +0800

    add  random code

--- src/main/src/controller/LoginController.java
+++ src/main/src/controller/LoginController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import com.jfinal.core.Controller;
+import com.jfinal.render.CaptchaRender;
 
 /**
  * Created by Administrator on 2016/7/4.
@@ -11,8 +12,20 @@ public class LoginController  extends Controller{
         renderJsp("/jsp/login.jsp");
     }
 
-    //登录按钮
+    // 登录按钮
     public void login(){
-        forwardAction("/welcome");
+        String randomCode = getPara("randomCode");
+        boolean loginSuccess = CaptchaRender.validate(this, randomCode);
+        if (loginSuccess) {
+            forwardAction("/welcome");
+        } else {
+            forwardAction("/welcome");
+        }
+    }
+
+    // 验证码
+    public void img() {
+        CaptchaRender img = new CaptchaRender();
+        render(img);
     }
 }
