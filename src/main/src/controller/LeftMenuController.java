d613d6f815a6c0821096e29246319364c11769bd
joorhy <joorhy@gmail.com>
2016-07-08 16:04:10 +0800

    aa

--- src/main/src/controller/LeftMenuController.java
+++ src/main/src/controller/LeftMenuController.java
@@ -7,13 +7,15 @@ import com.jfinal.core.Controller;
  */
 public class LeftMenuController extends Controller {
     // 页面初始化
-    public String index() {
+    public void index() {
         //查询待审批，已经审批，审批未通过数量
         int toApprove = 1;
         int approved = 2;
         int rejected = 3;
-        System.out.print("[{'toApprove':"+toApprove+",'approved':"+approved+",'rejected':"+rejected+"}]");
-        return "[{'toApprove':"+toApprove+",approved':"+approved+",'rejected':"+rejected+"}]";
+        setAttr("toApprove",toApprove);
+        setAttr("approved",approved);
+        setAttr("rejected",rejected);
+        renderJson();
 
     }
 }
