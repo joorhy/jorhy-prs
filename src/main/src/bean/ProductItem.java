a230141d0113af57a1e0b609bc7cbbce4e0262fe
joorhy <joorhy@gmail.com>
2016-08-02 22:22:00 +0800

    Add logback and ehcache

---
+++ src/main/src/bean/ProductItem.java
@@ -0,0 +1,14 @@
+package bean;
+
+/**
+ * Created by Joo on 2016/7/30.
+ */
+public class ProductItem {
+    public String strPrjName;
+    public int nPrjCount;
+    public double fPrjPrice;
+    public String strPrjSpec;
+    public double fPrjTotalPrice;
+    public String strPrjParam;
+    public String strPrjAttachFile;
+}
